f24a3515c5b2182a11d1224278d0eae23327de54
Filip Baba <Filip.Baba@rt-rk.com>
2026-03-25 09:59:40 +0100

    Implement 1st and 2nd exercise

--- 1-tdd/factorial.py
+++ 1-tdd/factorial.py
@@ -1,4 +1,11 @@
 # define your solution
 def factorial(n):
-    pass
-
+    # factorial(n) = n * factorial(n-1)
+    if n < 0:
+        raise ValueError("Input should be a non-negative integer")
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    result = n * factorial(n-1)
+    return result
--- 1-tdd/fibonacci.py
+++ 1-tdd/fibonacci.py
@@ -1,4 +1,12 @@
 # define your solution
 def fibonacci(n):
-    pass
+    # fibonacci(n-1) + fibonacci(n-2)
+    if n < 0:
+        raise ValueError("Input should be a non-negative integer")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    result = fibonacci(n-1) + fibonacci(n-2)
+    return result
 
